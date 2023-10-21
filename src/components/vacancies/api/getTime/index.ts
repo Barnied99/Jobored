c312c06718ff12f334d838a5e332e6d9a532894a
Valery_Savitski <darkbarnied99@gmail.com>
2023-10-21 23:09:26 +0300

    homepage

--- src/components/vacancies/api/getTime/index.ts
+++ src/components/vacancies/api/getTime/index.ts
@@ -1,7 +1,7 @@
 import { jobsApi } from '@/components/common/api';
-import { Catalogue } from '@/components/vacancies/types';
+// import { Catalogue } from '@/components/vacancies/types';
 
-export const getTime = async (): Promise<Catalogue[]> => {
+export const getTime = async () => {
     return await jobsApi.get('http://localhost:5232/api/2.0/references/').then((res) => res.data);
 };
 
