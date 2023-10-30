7183adb1e9fc7830deff0b45350a99e87ee2910c
Valery_Savitski <darkbarnied99@gmail.com>
2023-10-30 23:07:49 +0300

    add mobFilter to expi,typework

--- src/components/vacancies/components/Filters/validation.ts
+++ src/components/vacancies/components/Filters/validation.ts
@@ -4,4 +4,6 @@ export const filterSchema = yup.object({
 	catalogues: yup.string().optional().default(''),
 	payment_from: yup.string().optional().default(''),
 	payment_to: yup.string().optional().default(''),
+	expirience: yup.string().optional().default(''),
+	typework: yup.string().optional().default(''),
 });
