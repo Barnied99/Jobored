831332757d595a8c4a9f3fe054989dc42159f7d5
Valery_Savitski <darkbarnied99@gmail.com>
2023-11-08 22:41:37 +0300

    ssr to lcp, add sw

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -57,6 +57,12 @@ export const Document = () => {
   return (
     <Html lang="en">
       <Head >
+        <meta
+          name="description"
+          content="Build a PWA to achieve 100% in Google Lighthouse and Next.js Analytics"
+        />
+        <meta name="theme-color" content="#333333" />
+        <meta name="referrer" content={'strict-origin'} />
         <link rel="icon" href="/favicon.svg"></link>
         <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js" async></script>
       </Head>
--- src/utills/registerSW.ts
+++ src/utills/registerSW.ts
@@ -1,6 +1,6 @@
 export default async function registerSW() {
     try {
-        const reg = await navigator.serviceWorker.register('/sw.js')
+        const reg = await navigator.serviceWorker.register('http://localhost:3000/sw.js')
         console.log(`Registration scope: ${reg.scope}`)
     } catch (e) {
         console.log(e)
