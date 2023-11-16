6cea74ea2fc21463399520cc5d9e6a289d2bd23b
Valery_Savitski <darkbarnied99@gmail.com>
2023-11-16 23:23:01 +0300

    footer

--- src/components/common/component/ui/Footer/index.tsx
+++ src/components/common/component/ui/Footer/index.tsx
@@ -1,108 +1,97 @@
-import { Container, Text, Group, Paper } from '@mantine/core';
-import React from 'react';
-import Link from 'next/link';
+import Image from 'next/image';
+import { Text, Container, ActionIcon, Group, rem } from '@mantine/core';
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
+import { LogoFull } from '@/assets/img';
 
-import { useStyles } from './styles';
+import classes from './FooterLinks.module.css';
 
+const data = [
+    {
+        title: 'Партнерам',
+        links: [
+            { label: 'Реклама на сайте', link: 'https://www.superjob.ru/targetirovannaya_reklama/?utm_source=superjob&utm_campaign=desktop_footer' },
+            { label: 'Интеграционные сервисы', link: 'https://www.superjob.ru/integration/' },
+            { label: 'Продвижение вакансий', link: 'https://www.superjob.ru/reklama_vakansii/?utm_source=superjob&utm_campaign=desktop_footer' },
+        ],
+    },
+    {
+        title: 'SuperJob',
+        links: [
+            { label: 'О компании', link: 'https://www.superjob.ru/clients/superjob-14449.html' },
+            { label: 'SuperJob.tech', link: 'https://superjob.tech/' },
+            { label: 'Новости сервиса', link: 'https://www.superjob.ru/news/' },
+            { label: 'Работа в SuperJob', link: 'https://www.superjob.ru/clients/superjob-14449.html' },
+        ],
+    },
+    {
+        title: 'Документы',
+        links: [
+            { label: 'База данных SuperJob', link: 'https://www.superjob.ru/info/hr_service.html' },
+            { label: 'Рекламный кабинет', link: 'https://www.superjob.ru/info/rtb_service.html' },
+            { label: 'Агрегатор образовательных курсов', link: 'https://www.superjob.ru/info/course-license-agreement/' },
+            { label: 'Иные документы', link: 'https://www.superjob.ru/hr/billing/documents.html' },
+        ],
+    },
 
+];
 
-const Footer = (
-) => {
+export function Footer() {
+    const groups = data.map((group) => {
+        const links = group.links.map((link, index) => (
+            <Text<'a'>
+                key={index}
+                className={classes.link}
+                component="a"
+                href={link.link}
+            // onClick={(event) => event.preventDefault()}
+            >
+                {link.label}
+            </Text>
+        ));
 
-    const { classes } = useStyles();
+        return (
+            <div className={classes.wrapper} key={group.title}>
+                <Text className={classes.title}>{group.title}</Text>
+                {links}
+            </div>
+        );
+    });
 
     return (
-        <Container
-        >
-            <footer>
-                <Paper
-                    p={3}
-                    pb={11}
-                    pt={20}
-                >
-                    <Group className={classes.columnsWrapper}>
-                        <Group className={classes.columnsWrapperGroup}>
-                            <Text fw={700}>Партнерам</Text>
-                            <Link href='https://www.superjob.ru/targetirovannaya_reklama/?utm_source=superjob&utm_campaign=desktop_footer'>
-                                <Text c="dark">Реклама на сайте</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/integration/'>
-                                <Text c="dark">Интеграционные сервисы</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/reklama_vakansii/?utm_source=superjob&utm_campaign=desktop_footer'>
-                                <Text c="dark">Продвижение вакансий</Text>
-                            </Link>
-                        </Group>
-                        <Group className={classes.columnsWrapperGroup}>
-                            <Text fw={700}>SuperJob</Text>
-                            <Link href='https://www.superjob.ru/clients/superjob-14449.html'>
-                                <Text c="dark">О компании</Text>
-                            </Link>
-                            <Link href='https://superjob.tech/'>
-                                <Text c="dark">SuperJob.tech</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/news/'>
-                                <Text c="dark">Новости сервиса</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/clients/superjob-14449.html'>
-                                <Text c="dark">Работа в SuperJob</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/research/'>
-                                <Text c="dark">Исследования</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/proforientaciya-superjob/'>
-                                <Text c="dark">Профориентация</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/kalkulyatory/'>
-                                <Text c="dark">Калькуляторы</Text>
-                            </Link>
-                        </Group>
-                        <Group className={classes.columnsWrapperGroup}>
-                            <Text fw={700}>Документы</Text>
-                            <Link href='https://www.superjob.ru/info/hr_service.html'>
-                                <Text c="dark">База данных SuperJob</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/info/rtb_service.html'>
-                                <Text c="dark">Рекламный кабинет</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/info/course-license-agreement/'>
-                                <Text c="dark">Агрегатор образовательных курсов </Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/hr/billing/documents.html'>
-                                <Text c="dark">Иные документы </Text>
-                            </Link>
-                        </Group>
-                        <Group className={classes.columnsWrapperGroup}>
-                            <Text fw={700}>Работодателям</Text>
-                            <Link href='https://www.superjob.ru/hr/vacancy/create/'>
-                                <Text c="dark">Разместить вакансию</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/aboutsuperstart'>
-                                <Text c="dark">Найти стажера</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/hr/bill.html'>
-                                <Text c="dark">Тарифы</Text>
-                            </Link>
-                        </Group>
-                        <Group className={classes.columnsWrapperGroup}>
-                            <Text fw={700}>Соискателям</Text>
-                            <Link href='https://www.superjob.ru/resume/create/'>
-                                <Text c="dark">Создать резюме</Text>
-                            </Link>
-                            <Link href='https://www.superjob.ru/vakansii/'>
-                                <Text c="dark">Поиск работы</Text>
-                            </Link>
-                            <Link href='https://students.superjob.ru/'>
-                                <Text c="dark">Работа для студентов</Text>
-                            </Link>
-                        </Group>
-                    </Group>
-                </Paper>
-            </footer>
+        <footer className={classes.footer}>
+            <Container className={classes.inner}>
+                <div className={classes.logo}>
+                    <Image
+                        src={LogoFull}
+                        alt="Jobored Logo"
+                        className={classes.header__logo}
+                        priority
+                    />
+                    <Text size="xs" c="dimmed" className={classes.description}>
+                        Build fully functional accessible web applications faster than ever
+                    </Text>
+                </div>
+                <div className={classes.groups}>{groups}</div>
+            </Container>
+            <Container className={classes.afterFooter}>
+                <Text c="dimmed" size="sm">
+                    © 2023 Savitskiy.mantine.dev All rights reserved.
+                </Text>
 
-
-        </Container>
+                <Group className={classes.social} align="flex-end" noWrap>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+                    </ActionIcon>
+                </Group>
+            </Container>
+        </footer>
     );
-};
+}
 
 export default Footer;
