3e0bfe747d4552ad8f5a9acf32e21f49fa7ac453
Valery_Savitski <darkbarnied99@gmail.com>
2023-12-06 20:24:49 +0300

    namefix

--- src/components/common/component/layout/DefaultLayout/styles.ts
+++ src/components/common/component/layout/DefaultLayout/styles.ts
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export const bounce = keyframes({
 	'from, 20%, 53%, 80%, to': { transform: 'translate3d(0, 0, 0)' },
-	'40%, 43%': { transform: 'translate3d(0, -1.475rem, 0)' },
+	'40%, 42%': { transform: 'translate3d(0, -1.475rem, 0)' },
 	'70%': { transform: 'translate3d(0, -0.9375rem, 0)' },
 	'90%': { transform: 'translate3d(0, -0.25rem, 0)' },
 });
