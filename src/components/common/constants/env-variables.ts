6ee1d4ed077217af535854494feecbb5dffe3d8b
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-04-28 14:58:50 +0300

    remove details auth

--- src/components/common/constants/env-variables.ts
+++
@@ -1,5 +0,0 @@
-export const NEXT_JOBS_API_URL = "https://api.superjob.ru";
-export const NEXT_LOGIN = "darkbarnied99@gmail.com";
-export const NEXT_PASSWORD = "DFio3380";
-export const NEXT_CLIENT_ID = "2547";
-export const NEXT_CLIENT_SECRET = "v3.r.137589215.fcf4c49c52536b90f4627b136495a9ba55600c5d.83d03895eee6bdcc01e9104ccc143e6af6c9263c";
